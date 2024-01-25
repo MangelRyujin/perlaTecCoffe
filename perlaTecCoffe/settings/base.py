94dc8b1cbee1869c263125bb4031346d1ec9ac88
Miguel Angel Fernández Rodríguez <mangeldeveloper99@gmail.com>
2024-01-25 09:47:43 -0500

    modified base.py settings

--- perlaTecCoffe/settings/base.py
+++ perlaTecCoffe/settings/base.py
@@ -94,14 +94,16 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
+    "*",
 ]
 
 
 CORS_ORIGIN_WHITELIST =[
     "http://localhost:3000",
     "http://localhost",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
+    "*",
 ]
 
 
